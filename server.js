3f91829288c5dc2a24f289a16375a24b09997494
FtBishal <ftbishal47@gmail.com>
2025-04-21 21:01:58 +0530

    Fix frontend

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json()); // For parsing JSON bodies
 app.use(cors({
-  origin: "https://thriving-rugelach-060979.netlify.app"
+  origin: "https://wishland.netlify.app"
 }));
 
 
